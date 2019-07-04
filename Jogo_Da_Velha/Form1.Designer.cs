d9a2ce9f45a767862c127ff93bd5049f25c99504
Rafael Lima <rafael@ntsinfo.com.br>
2019-07-04 11:49:26 -0300

    First commit

---
+++ Jogo_Da_Velha/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Jogo_Da_Velha
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            resources.ApplyResources(this.btn00, "btn00");
+            this.btn00.Name = "btn00";
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn01
+            // 
+            resources.ApplyResources(this.btn01, "btn01");
+            this.btn01.Name = "btn01";
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn02
+            // 
+            resources.ApplyResources(this.btn02, "btn02");
+            this.btn02.Name = "btn02";
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn12
+            // 
+            resources.ApplyResources(this.btn12, "btn12");
+            this.btn12.Name = "btn12";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn11
+            // 
+            resources.ApplyResources(this.btn11, "btn11");
+            this.btn11.Name = "btn11";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn10
+            // 
+            resources.ApplyResources(this.btn10, "btn10");
+            this.btn10.Name = "btn10";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn22
+            // 
+            resources.ApplyResources(this.btn22, "btn22");
+            this.btn22.Name = "btn22";
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn21
+            // 
+            resources.ApplyResources(this.btn21, "btn21");
+            this.btn21.Name = "btn21";
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn20
+            // 
+            resources.ApplyResources(this.btn20, "btn20");
+            this.btn20.Name = "btn20";
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btn22);
+            this.groupBox1.Controls.Add(this.btn11);
+            this.groupBox1.Controls.Add(this.btn02);
+            this.groupBox1.Controls.Add(this.btn00);
+            this.groupBox1.Controls.Add(this.btn20);
+            this.groupBox1.Controls.Add(this.btn10);
+            this.groupBox1.Controls.Add(this.btn12);
+            this.groupBox1.Controls.Add(this.btn21);
+            this.groupBox1.Controls.Add(this.btn01);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // btnLimpar
+            // 
+            resources.ApplyResources(this.btnLimpar, "btnLimpar");
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmPrincipal
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmPrincipal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Jogo_Da_Velha/Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_Da_Velha
+{
+   
+    public partial class frmPrincipal : Form
+    {
+        public char vez = 'X';
+        char[,] tabela = new char[3, 3];
+        bool terminou = false;
+        int x, y;
+        int cont = 0;
+        int pontosX = 0, pontosO = 0;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            groupBox1.Focus();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            x = Convert.ToInt32(btn.Name[3].ToString());
+            y = Convert.ToInt32(btn.Name[4].ToString());
+            btn.Enabled = false;
+            if (vez == 'X')
+            {
+                tabela[x, y] = vez;
+                btn.Text = vez.ToString();
+                Valida(vez);
+                vez = 'O';
+                
+            }
+            else
+            {
+                tabela[x, y] = vez;
+                btn.Text = vez.ToString();
+                Valida(vez);
+                vez = 'X';
+
+
+            }
+
+
+
+            //btnLimpar.Focus();
+            groupBox1.Focus();
+
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+            limpa();
+            pontosO = 0;
+            pontosX = 0;
+            groupBox1.Text = "X: 0 VS O: 0";
+
+
+        }
+
+
+        void Valida(char vez)
+        {
+            int encontra = 0;
+            cont++;
+            int onde_ganhou = 0;
+            //primeira linha
+            for (int x = 0; x < 3; x++)
+                {
+
+                    if (tabela[0, x] == vez)
+                    {
+                        encontra++;
+
+                    }
+
+                }
+
+                if (encontra == 3)
+                {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 1;
+
+                    
+                }
+
+            encontra = 0;
+            //segunda linha
+            for (int x = 0; x < 3; x++)
+            {
+
+                if (tabela[1, x] == vez)
+                {
+                    encontra++;
+
+                }
+
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 2;
+
+
+            }
+
+
+            encontra = 0;
+            //terceira linha
+            for (int x = 0; x < 3; x++)
+            {
+
+                if (tabela[2, x] == vez)
+                {
+                    encontra++;
+
+                }
+
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 3;
+
+
+            }
+
+
+            encontra = 0;
+            //primeira coluna
+            for (int x = 0; x < 3; x++)
+            {
+
+                if (tabela[x, 0] == vez)
+                {
+                    encontra++;
+
+                }
+
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 4;
+
+
+            }
+
+
+            encontra = 0;
+            //segunda coluna
+            for (int x = 0; x < 3; x++)
+            {
+
+                if (tabela[x, 1] == vez)
+                {
+                    encontra++;
+
+                }
+
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 5;
+
+
+            }
+
+
+
+            encontra = 0;
+            //terceira coluna
+            for (int x = 0; x < 3; x++)
+            {
+
+                if (tabela[x, 2] == vez)
+                {
+                    encontra++;
+
+                }
+
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 6;
+
+
+            }
+
+            encontra = 0;
+            //diagonal1
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (x == y)
+                    {
+                        if (tabela[x, y] == vez)
+                        {
+                            encontra++;
+
+                        }
+                    }
+                }
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 7;
+
+
+            }
+
+            encontra = 0;
+            //diagonal2
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (x + y == 2)
+                    {
+                        if (tabela[x, y] == vez)
+                        {
+                            encontra++;
+
+                        }
+                    }
+                }
+            }
+
+            if (encontra == 3)
+            {
+
+                // MessageBox.Show(vez + " ganhou");
+                // limpa();
+                onde_ganhou = 8;
+
+
+            }
+
+
+
+
+
+
+
+
+            if (onde_ganhou == 1)
+            {
+                MessageBox.Show(vez + " ganhou na primeira linha!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+
+            }
+            if (onde_ganhou == 2)
+            {
+                MessageBox.Show(vez + " ganhou na segunda linha!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if(onde_ganhou == 3)
+            {
+                MessageBox.Show(vez + " ganhou na terceira linha!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 4)
+            {
+                MessageBox.Show(vez + " ganhou na primeira coluna!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 5)
+            {
+                MessageBox.Show(vez + " ganhou na segunda coluna!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 6)
+            {
+                MessageBox.Show(vez + " ganhou na terceira coluna!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 7)
+            {
+                MessageBox.Show(vez + " ganhou na diagona 1!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 8)
+            {
+                MessageBox.Show(vez + " ganhou na diagonal2!");
+                if (vez == 'X')
+                {
+                    pontosX++;
+                }
+                else
+                {
+
+                    pontosO++;
+                }
+                groupBox1.Text = "X: " + pontosX + " VS O: " + pontosO;
+                limpa();
+            }
+            if (onde_ganhou == 0 && cont == 9)
+            {
+                MessageBox.Show("Deu velha!");
+                limpa();
+            }
+
+
+
+            }
+           
+
+           
+        
+
+        void limpa()
+        {
+            btn00.Text = "";
+            btn01.Text = "";
+            btn02.Text = "";
+            btn10.Text = "";
+            btn11.Text = "";
+            btn12.Text = "";
+            btn20.Text = "";
+            btn21.Text = "";
+            btn22.Text = "";
+            btn00.Enabled = true;
+            btn01.Enabled = true;
+            btn02.Enabled = true;
+            btn10.Enabled = true;
+            btn11.Enabled = true;
+            btn12.Enabled = true;
+            btn20.Enabled = true;
+            btn21.Enabled = true;
+            btn22.Enabled = true;
+            cont = 0;
+            for(int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    tabela[x, y] = 'V';
+                }
+            }
+            vez = 'X';
+        }
+
+    }
+}
